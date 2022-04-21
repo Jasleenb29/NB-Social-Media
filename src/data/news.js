07bbb1e2cf8c14cc7f92e27171de5ca5e94bcf52
Jasleen Bhatia <jasleenbhatia@Jasleens-Air.mshome.net>
2022-04-21 16:40:26 +0530

    major hardcoded data added

---
+++ src/data/news.js
@@ -0,0 +1,15 @@
+const news = [
+  {
+    image: "https://girlup.imgix.net/2021/10/TY88udVP-1.png?auto=format&fit=crop&h=250&ixlib=php-3.3.0&w=500&wpsize=card",
+    title: "Lorem ipsum dolor sit amet",
+    description:"Morbi leo mi, nonummy eget, tristique non, rhoncus non, leo. Nullam faucibus mi quis velit. Integer in sapien. Fusce tellus odio, dapibus id, fermentum quis, suscipit id, erat. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat. Pellentesque sapien. Cras elementum. Nulla pulvinar eleifend sem. Cum sociis natoque penatibus et magnis dis",
+  },
+  {
+    image: "https://www.eskills4girls.org/wp-content/uploads/2019/02/womenintech-300x220.png",
+    title: "Women in Tech",
+    description:"Morbi leo mi, nonummy eget, tristique non, rhoncus non, leo. Nullam faucibus mi quis velit. Integer in sapien. Fusce tellus odio, dapibus id, fermentum quis, suscipit id, erat. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat. Pellentesque sapien. Cras elementum. Nulla pulvinar eleifend sem. Cum sociis natoque penatibus et magnis dis",
+  },
+  
+];
+
+export default news;
